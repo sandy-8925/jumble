7350af2de809c8fceed02e2bfe9a6c4e56731d6e
Sandeep Raghuraman <sandy.8925@gmail.com>
2014-03-19 16:57:49 -0700

    WordSetComputer: Improve documentation

--- src/org/sanpra/jumble/WordSetComputers/WordSetComputer.java
+++ src/org/sanpra/jumble/WordSetComputers/WordSetComputer.java
@@ -3,7 +3,7 @@ package org.sanpra.jumble.WordSetComputers;
 import java.util.Set;
 
 /**
- * Common interface for any strategy to compute set of valid words given an input string
+ * Common interface for any strategy to compute set of valid words from characters of a string
  */
 public interface WordSetComputer {
     public Set<String> compute(String inputString);
